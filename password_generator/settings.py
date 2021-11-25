05e66a32d955873cf25218f1a8b42911f4a555b6
David Lindblom <daavidlindblom@gmail.com>
2021-11-25 13:53:20 +0100

    commit 2
    2nd commit

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from . import API_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(zdim+d(xp26c2sft83e@u1i!^vc%d*)ncdl7m830z1h_=^ypj'
+SECRET_KEY = API_KEY.API_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
